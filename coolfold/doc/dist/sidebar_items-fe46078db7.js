67a3272560089fbfc81d51bdfb03d4fad3316438
Sayan <7rat13@gmail.com>
2021-06-13 13:36:59 +0530

    Remove /doc from .gitignore.

---
+++ coolfold/doc/dist/sidebar_items-fe46078db7.js
@@ -0,0 +1,1 @@
+sidebarNodes={"extras":[{"group":"","headers":[{"anchor":"modules","id":"Modules"}],"id":"api-reference","title":"API Reference"}],"modules":[{"group":"","id":"Coolfold","nodeGroups":[{"key":"functions","name":"Functions","nodes":[{"anchor":"foldl/3","id":"foldl/3"},{"anchor":"foldr/3","id":"foldr/3"}]}],"sections":[],"title":"Coolfold"}],"tasks":[]}
